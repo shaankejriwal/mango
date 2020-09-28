d5430e7357ba84346edaa9597d98888fe4da1b5c
shaankejriwal <70429914+shaankejriwal@users.noreply.github.com>
2020-09-28 15:11:23 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,11 @@
+class mango extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("mango.png");
+      
+    }
+  
+    display() {
+      super.display();
+    }
+  }
---
+++ tree.js
@@ -0,0 +1,10 @@
+class Tree extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("tree.png");
+  }
+
+  display() {
+    super.display();
+  }
+}
